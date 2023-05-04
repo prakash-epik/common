905ef243bee1ec5bd7ff9039235c2efd6b82ccce
Sanju Sabu <sanju.sabu@epikso.com>
2023-05-04 16:30:49 +0530

    modified

--- my-app/src/app/app.component.spec.ts
+++ my-app/src/app/app.component.spec.ts
@@ -25,7 +25,7 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello World');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello World!');
   });
   
 });
